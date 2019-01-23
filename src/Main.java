229c7e663e7e0d8878585a92d3fa5618399b7d4f
OstTuz <46460127+OstTuz@users.noreply.github.com>
2019-01-23 17:02:18 +0200

    Changes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello its me");
+        Employee employee = new Employee("James", 1234, 2000);
     }
 }
